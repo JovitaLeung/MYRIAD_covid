87e8e6fd078bbeaea55d05b4cc47e1a12617cc0b
Jovita Leung <jovita.leung@ucl.ac.uk>
2021-03-23 17:14:16 +0000

    initial commit

---
+++ preprocessing_JL.R
@@ -0,0 +1,7 @@
+### MYRIAD COVID Follow-up: Data preprocessing
+### Script by Jovita Leung 
+
+
+rm(list = ls())
+
+d=read.csv("~/Dropbox/MYRIAD Data/R_analysis/intervention analysis/covid_analysis/myriad_covid_raw_age.csv")
