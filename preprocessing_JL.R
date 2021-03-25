401847f53d52cec67320b0c18952da50e607038f
Jovita Leung <jovita.leung@ucl.ac.uk>
2021-03-25 11:31:41 +0000

    update preprocessing script

--- preprocessing_JL.R
+++ preprocessing_JL.R
@@ -13,39 +13,43 @@ library(ggplot2)
 d=read.csv("~/Dropbox/MYRIAD Data/R_analysis/intervention analysis/covid_analysis/myriad_covid_raw_age.csv")
 d=d[-1,]
 
+s=read.csv("~/Dropbox/MYRIAD Data/R_analysis/intervention analysis/SI analysis/all_data_2021.csv")
+s$X=NULL
+# si=subset(s, select = c("subjno","session","delta_rating","R2R1diff","abs.R2R1diff","intervention","sdq_pro_sum"))
+# summary(si)
+t1=subset(s,s$session==1)
+t2=subset(s,s$session==2)
+
+
+## Data extraction --- 
+# Extract demographics 
 demo=subset(d, select = c("subjno","gender","age","dob"))
 demo[c("subjno","gender","age")]=lapply(demo[c("subjno","gender","age")], as.numeric)
+demo[c("subjno","gender")]=lapply(demo[c("subjno","gender")], factor)
 demo$dob=as.Date(as.character(parse_date_time(demo$dob, orders="dmY")))
 summary(demo)
 str(demo)
 
-c=subset(d,select = c("subjno","Q84","Q85","Q86")) #compliance to COVID measures (social distancing, mask wearing, preventive measures)
-c[c("subjno","Q84","Q85","Q86")]=lapply(c[c("subjno","Q84","Q85","Q86")], as.numeric) #1=not at all compliant, 5=completely
-summary(c)
-names(c)=c("subjno","comply_distance","comply_mask","comply_prev")
-
-s=read.csv("~/Dropbox/MYRIAD Data/R_analysis/intervention analysis/SI analysis/all_data_2021.csv")
-si=subset(s, select = c("subjno","session","delta_rating","R2R1diff","abs.R2R1diff","intervention","sdq_pro_sum"))
-summary(si)
-si_t1=subset(si,si$session==1)
-si_t2=subset(si,si$session==2)
-
-covid=merge(demo,c, by="subjno")
-covid1=merge(covid,si_t1, by="subjno")
-summary(covid1)
-
-options(scipen = 999) #convert scientific notation to decimal
+# Extract relevant covid questions
+covid=subset(d,select = c("subjno","Q84","Q85","Q86",
+                      "Q78","Q79","support_change_1","support_change_2","support_change_3","support_change_4","support_change_5")) 
+covid=as.data.frame(sapply(covid,as.numeric))
+colnames(covid)=c("subjno","comply_distance","comply_mask","comply_prev",
+           "connect_friends","support_friends",
+           "change_support_friends","change_support_fam","share_feel_friends","share_feel_fam","care_feel")
+fact=c("subjno","comply_distance","comply_mask","comply_prev","connect_friends","support_friends")
+covid[fact]=lapply(covid[fact], factor)
+summary(covid)
 
 
+## Merge data --- 
+covid_all=merge(demo,covid, by="subjno")
+si_covid_t1=merge(t1, covid_all, by="subjno")
+si_covid_t2=merge(t2, covid_all, by="subjno")
 
-## Correlations: SDQ & COVID ---
-cor.test(covid1$sdq_pro_sum,covid1$comply_distance) #Yes. r = 0.14, p < .001
-cor.test(covid1$sdq_pro_sum,covid1$comply_mask)#Yes. r = 0.05, p = .002
-cor.test(covid1$sdq_pro_sum,covid1$comply_prev) #Yes. r = 0.19, p < .001
+## Saving CSVs ---
+write.csv(covid_all, file = "~/Dropbox/MYRIAD Data/R_analysis/intervention analysis/covid_analysis/covid_demo.csv")
+write.csv(si_covid_t1, file = "~/Dropbox/MYRIAD Data/R_analysis/intervention analysis/covid_analysis/si_covid_t1.csv")
+write.csv(si_covid_t2, file = "~/Dropbox/MYRIAD Data/R_analysis/intervention analysis/covid_analysis/si_covid_t2.csv")
 
-## Correlations: R2R1diff at T1 & COVID --- 
-cor.test(covid1$abs.R2R1diff,covid1$comply_distance) #Yes. r = -0.06, p < .001
-cor.test(covid1$abs.R2R1diff,covid1$comply_mask) #No. p = .574
-cor.test(covid1$abs.R2R1diff,covid1$comply_prev) #No. p = .226
 
-ggplot(covid1, aes(x=comply_distance, y=sdq_pro_sum))+geom_point()+geom_smooth(method = 'lm')+theme_classic()
